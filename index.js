47e4e448f2bf13891353816d4ead18598d7d93a7
Kris de Rycke <derycke.kris@gmail.com>
2019-05-29 16:21:37 +0200

    add array for weekdays

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-document.getElementById("body").addEventListener("load", myFunction);
+var d = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friyay";
+weekday[6] = "Saturday";
 
-function myFunction() {
-  var x = getDay();
-
-  console.log(x);
-}
+var n = weekday[d.getDay()];
+console.log(n);
