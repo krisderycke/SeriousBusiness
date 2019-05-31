e0f5abc7dfb339df23e004249ea624c5d48ff7eb
Kris de Rycke <derycke.kris@gmail.com>
2019-05-31 09:59:17 +0200

    get the day of the week shown on htmldoc

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-var d = new Date();
-var weekday = Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friyay";
-weekday[6] = "Saturday";
+function myTime() {
+  var d = new Date();
+  var weekday = Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friyay";
+  weekday[6] = "Saturday";
 
-var n = weekday[d.getDay()];
-console.log(n);
+  var n = weekday[d.getDay()];
+  console.log(n);
 
-if(n==)
+  if () {
+    console.log("closed");
+  }
+
+  document.getElementById("currentTime").innerHTML = n;
+}
